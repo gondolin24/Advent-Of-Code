80dbb1341da0c554a620ca14dc5128b6b948016c
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-12-09 22:34:21 -0500

    not my greatest day

--- advent-of-code/2020/day_8/day_8_solution.js
+++ advent-of-code/2020/day_8/day_8_solution.js
@@ -15,18 +15,82 @@ class GameBoy {
         this.acc = 0
     }
 
+    fixBug() {
+        let instructionIndex = 0
+        let currentInstruction = this.getCurrentInstruction(0)
+        const set = new Set()
+        let bugFixed = false
+        while (instructionIndex !== this.instructions.length) {
+
+            if (!bugFixed) {
+                const tempI = this.acc
+                const {bug, newVal} = this.runBugTest(instructionIndex)
+                bugFixed = bug
+                currentInstruction = this.getCurrentInstruction(instructionIndex)
+
+                if (bugFixed) {
+                    currentInstruction.instruction = newVal
+                }
+                this.acc = tempI
+            }
+            set.add(instructionIndex.toString())
+            const {index, instruction} = this.getPerformInstruction(currentInstruction, instructionIndex)
+            instructionIndex = index
+            currentInstruction = instruction
+        }
+        return this.acc
+    }
+
+    runBugTest(ii) {
+        let instructionIndex = ii
+        let currentInstruction = this.getCurrentInstruction(ii)
+        let newVal = ''
+        switch (currentInstruction.instruction) {
+            case 'nop': {
+                newVal = 'jmp'
+                currentInstruction.instruction = 'jmp'
+                break
+            }
+            case 'jmp': {
+                newVal = 'nop'
+                currentInstruction.instruction = 'nop'
+                break
+            }
+            default: {
+                return {
+                    bug: false,
+                    newVal: null
+                }
+            }
+        }
+
+
+        const set = new Set()
+        while (!set.has(instructionIndex.toString()) && instructionIndex !== this.instructions.length) {
+            set.add(instructionIndex.toString())
+            const {index, instruction} = this.getPerformInstruction(currentInstruction, instructionIndex)
+            instructionIndex = index
+            currentInstruction = instruction
+        }
+
+        return {
+            bug: this.instructions.length === instructionIndex,
+            newVal
+        }
+
+    }
+
+
     runGame() {
         let instructionIndex = 0
         let currentInstruction = this.getCurrentInstruction(0)
         const set = new Set()
         while (!set.has(instructionIndex.toString())) {
             set.add(instructionIndex.toString())
-
             const {index, instruction} = this.getPerformInstruction(currentInstruction, instructionIndex)
             instructionIndex = index
             currentInstruction = instruction
         }
-
         return this.acc
     }
 
@@ -61,18 +125,16 @@ class GameBoy {
         }
     }
 
-
     getCurrentInstruction(index) {
-        return this.instructions[index]
+        let gg = {}
+        Object.assign(gg, this.instructions[index])
+        return gg
     }
 
 }
 
-
-function
-
-solvePartOne() {
-    const data = importFile().map((a) => {
+function getData() {
+    return importFile().map((a) => {
         const [instruction, value] = a.split(' ')
         let currVal = value
         if (value.includes('+')) {
@@ -84,10 +146,21 @@ solvePartOne() {
             value: Number(currVal)
         }
     })
+}
+
+function solvePartOne() {
+    const data = getData()
     const game = new GameBoy(data)
     game.runGame()
     console.log(game.acc)
+}
 
+function solvePartTwo() {
+    const data = getData()
+    const game = new GameBoy(data)
+    game.fixBug()
+    console.log(game.acc)
 }
 
 solvePartOne()
+solvePartTwo()
